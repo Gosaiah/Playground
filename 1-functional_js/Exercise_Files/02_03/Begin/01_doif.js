876a9c4c8848c7d6a8950ee768339b0130c6c425
Isaiah <i.gosaiah@gmail.com>
2018-11-27 16:10:51 -0500

    intro tuts started

---
+++ 1-functional_js/Exercise_Files/02_03/Begin/01_doif.js
@@ -0,0 +1,32 @@
+// Learning Functional Programming with Javascript
+// Chapter 02, Video 03, Exercise 01
+
+function doIf(condition, func){
+	if(condition) {
+		func()
+	}
+}
+
+const x = 1
+
+doIf(x === 1, sayXis1)
+/*
+if (x === 1) {
+  sayXis1()
+}*/
+
+doIf(x === "Bananas", sayXisBananas)
+/*
+if (x === "Bananas") {
+  sayXisBananas()
+}*/
+
+if (x < 10 && x > 0) {
+  sayXisBetween0And10()
+}
+
+function sayXis1() { console.log("x is equal to 1") }
+
+function sayXisBananas() { console.log("x is equal to 'Bananas'") }
+
+function sayXisBetween0And10() { console.log("x is between 0 and 10") }
